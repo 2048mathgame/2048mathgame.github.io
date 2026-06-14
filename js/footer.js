eaaebaafa6a9898f241df7700ac638774b7840e6
2048mathgame <secretpics1989@duck.com>
2026-06-14 22:09:33 +0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -12,28 +12,29 @@
         <div class="footer-col">
           <h4>Game</h4>
           <ul>
-            <li><a href="#hero">Play Now</a></li>
-            <li><a href="#how-to-play">How to Play</a></li>
-            <li><a href="#strategies">Strategies</a></li>
-            <li><a href="#controls">Controls</a></li>
+            <li><a href="/#hero">Play Now</a></li>
+            <li><a href="/#how-to-play">How to Play</a></li>
+            <li><a href="/#strategies">Strategies</a></li>
+            <li><a href="/#controls">Controls</a></li>
           </ul>
         </div>
         <div class="footer-col">
           <h4>Learn</h4>
           <ul>
-            <li><a href="#tile-values">Math Behind Tiles</a></li>
-            <li><a href="#math-benefits">Math Benefits</a></li>
-            <li><a href="#features">Features</a></li>
-            <li><a href="#faq">FAQ</a></li>
+            <li><a href="/#tile-values">Math Behind Tiles</a></li>
+            <li><a href="/#math-benefits">Math Benefits</a></li>
+            <li><a href="/#features">Features</a></li>
+            <li><a href="/#faq">FAQ</a></li>
           </ul>
         </div>
         <div class="footer-col">
           <h4>About</h4>
           <ul>
-            <li><a href="#">About the Game</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms of Use</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/privacy">Privacy Policy</a></li>
+            <li><a href="/terms">Terms of Use</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/cookies">Cookies Policy</a></li>
           </ul>
         </div>
       </div>
